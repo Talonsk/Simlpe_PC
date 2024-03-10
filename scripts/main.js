c37f6a785968da083d6c3c1cead3e4c03f60bc54
Your name <email@example.com>
2024-03-10 19:06:30 +0300

    Update-2.3

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,12 @@
-let cartCounter = document.querySelector('#cart_counter')
+let cartCounter = document.querySelector('#cart_counter');
+let inCartButtom = document.querySelectorAll('#in_cart')
+let counter_value = 0
+
+for (i=0; i<=5; i++){
+    inCartButtom[i].addEventListener('click', function(e){
+        counter_value += 1
+        cartCounter.querySelector('p').innerText = counter_value
+        cartCounter.style.display = 'flex'
+        console.log(counter_value)
+    }, {once: true})
+};
