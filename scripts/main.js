391e38c205dda8f86f7096a29133d50c92cea951
Your name <email@example.com>
2024-03-11 11:05:03 +0300

    Update-3

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,14 @@
 let cartCounter = document.querySelector('#cart_counter');
-let inCartButtom = document.querySelectorAll('#in_cart')
-let counter_value = 0
+let inCartButtom = document.querySelectorAll('#in_cart');
+let counter_value = 0;
 
 for (i=0; i<=5; i++){
-    inCartButtom[i].addEventListener('click', function(e){
-        counter_value += 1
-        cartCounter.querySelector('p').innerText = counter_value
-        cartCounter.style.display = 'flex'
-        console.log(counter_value)
-    }, {once: true})
+    inCartButtom[i].addEventListener('click', showCartCounter.bind(null, i), {once: true});
 };
+
+function showCartCounter(x){
+    counter_value += 1;
+    cartCounter.querySelector('p').innerText = counter_value;
+    cartCounter.style.display = 'flex';
+    inCartButtom[x].style.backgroundColor = 'rgba(179, 235, 57, 0.4)';
+}
