4015c2d88184be0255632a095142efd13b66326b
Your name <email@example.com>
2024-03-14 22:35:09 +0300

    Update-3.1

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,30 @@
 let cartCounter = document.querySelector('#cart_counter');
 let inCartButtom = document.querySelectorAll('#in_cart');
+let submitOrder = document.querySelector('#submit_order')
 let counter_value = 0;
+
+if (!localStorage.getItem("counter")){
+    localStorage.setItem("counter", "0");
+}else if(localStorage.getItem("counter") != "0"){
+    cartCounter.querySelector('p').innerText = parseInt(localStorage.getItem("counter"));
+    cartCounter.style.display = 'flex';
+}
 
 for (i=0; i<=5; i++){
     inCartButtom[i].addEventListener('click', showCartCounter.bind(null, i), {once: true});
 };
 
 function showCartCounter(x){
-    counter_value += 1;
-    cartCounter.querySelector('p').innerText = counter_value;
+    counter_value = parseInt(localStorage.getItem("counter"));
+    localStorage.setItem("counter", counter_value + 1)
+    
+    cartCounter.querySelector('p').innerText = parseInt(localStorage.getItem("counter"));
     cartCounter.style.display = 'flex';
-    inCartButtom[x].style.backgroundColor = 'rgba(179, 235, 57, 0.4)';
 }
+
+submitOrder.addEventListener('click', function(e){
+    localStorage.setItem("counter", "0")
+    cartCounter.querySelector('p').innerText = parseInt(localStorage.getItem("counter"));
+    cartCounter.style.display = 'none';
+})
+
